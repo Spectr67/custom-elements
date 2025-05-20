da147e2d0ae427baeb5c082209c1c364eaeefec5
Arthur <pennywennyweed@gmail.com>
2025-05-20 23:00:49 +0300

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <script>
 import MButton from './components/MyButton.vue'
+import Navbar from './components/Navbar.vue'
+import MForm from './components/MForm.vue'
 
 export default {
   components: {
     MButton,
+    Navbar,
+    MForm,
+  },
+  data() {
+    return {
+      showForm: false,
+    }
+  },
+  methods: {
+    toggleForm() {
+      this.showForm = !this.showForm
+    },
   },
 }
 </script>
 
 <template>
+  <Navbar />
   <section>
     <article>
-      <MButton caption="Логин" class="disabled yo" />
+      <MButton @click="toggleForm" caption="Логин" class="yo" />
     </article>
   </section>
+
+  <MForm v-if="showForm" @close="hideForm" />
 </template>
 
 <style scoped>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+import MButton from './MyButton.vue'
+export default {
+  components: {
+    MButton,
+  },
+  props: {
+    caption: '',
+    link: '',
+  },
+}
+</script>
+
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><MButton caption="link1" link="https://google.com" /></li>
+        <li><MButton caption="link2" link="https://google.com" /></li>
+        <li><MButton caption="link3" link="https://google.com" /></li>
+      </ul>
+    </div>
+  </nav>
+</template>
