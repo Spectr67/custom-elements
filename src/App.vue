d272fe73c635f3be8bb8d470a570c4cf0599a1b6
Arthur <pennywennyweed@gmail.com>
2025-05-20 16:34:22 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import MButton from './components/MyButton.vue'
+
+export default {
+  components: {
+    MButton,
+  },
+}
+</script>
+
+<template>
+  <section>
+    <article>
+      <MButton caption="Логин" class="disabled yo" />
+    </article>
+  </section>
+</template>
+
+<style scoped>
+article {
+  padding: 25px;
+}
+</style>
